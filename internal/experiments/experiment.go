fe1e39756c504181d45cda01973a2ad94ea389e4
Martin Atkins <mart@degeneration.co.uk>
2024-02-29 11:06:44 -0800

    experiments: "ephemeral_values" language experiment
    This doesn't actually do anything yet, but in future commits it will
    enable some new language features for marking input variables and output
    values as "ephemeral", meaning that they don't get saved as part of
    state snapshots or saved plan files.

--- internal/experiments/experiment.go
+++ internal/experiments/experiment.go
@@ -17,6 +17,7 @@ type Experiment string
 // identifier so that it can be specified in configuration.
 const (
 	VariableValidation             = Experiment("variable_validation")
+	EphemeralValues                = Experiment("ephemeral_values")
 	ModuleVariableOptionalAttrs    = Experiment("module_variable_optional_attrs")
 	SuppressProviderSensitiveAttrs = Experiment("provider_sensitive_attrs")
 	ConfigDrivenMove               = Experiment("config_driven_move")
@@ -28,6 +29,7 @@ func init() {
 	// Each experiment constant defined above must be registered here as either
 	// a current or a concluded experiment.
 	registerCurrentExperiment(UnknownInstances)
+	registerCurrentExperiment(EphemeralValues)
 	registerConcludedExperiment(VariableValidation, "Custom variable validation can now be used by default, without enabling an experiment.")
 	registerConcludedExperiment(SuppressProviderSensitiveAttrs, "Provider-defined sensitive attributes are now redacted by default, without enabling an experiment.")
 	registerConcludedExperiment(ConfigDrivenMove, "Declarations of moved resource instances using \"moved\" blocks can now be used by default, without enabling an experiment.")
