68a3abe056eff0e48e6b6dfa1e51cd862e8b835b
UKEME BASSEY <ukeme.bassey@hashicorp.com>
2024-03-28 01:06:24 -0400

    fixing tests

--- internal/command/test_test.go
+++ internal/command/test_test.go
@@ -1923,8 +1923,8 @@ calculate lock file checksums locally for the following providers:
   - hashicorp/test
 
 The current .terraform.lock.hcl file only includes checksums for linux_amd64,
-so Terraform running on another platform will fail to install
-these providers.
+so Terraform running on another platform will fail to install these
+providers.
 
 To calculate additional checksums for another platform, run:
   terraform providers lock -platform=linux_amd64
@@ -2031,8 +2031,8 @@ calculate lock file checksums locally for the following providers:
   - hashicorp/test
 
 The current .terraform.lock.hcl file only includes checksums for linux_amd64,
-so Terraform running on another platform will fail to install
-these providers.
+so Terraform running on another platform will fail to install these
+providers.
 
 To calculate additional checksums for another platform, run:
   terraform providers lock -platform=linux_amd64
@@ -2106,8 +2106,8 @@ calculate lock file checksums locally for the following providers:
   - hashicorp/test
 
 The current .terraform.lock.hcl file only includes checksums for linux_amd64,
-so Terraform running on another platform will fail to install
-these providers.
+so Terraform running on another platform will fail to install these
+providers.
 
 To calculate additional checksums for another platform, run:
   terraform providers lock -platform=linux_amd64
